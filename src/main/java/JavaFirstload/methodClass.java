541a2a09e6802639302cdc8c685dcac58b80c179
Simon <sai@gmail.com>
2025-09-07 18:21:30 -0400

    Fourth  Commit

---
+++ src/main/java/JavaFirstload/methodClass.java
@@ -0,0 +1,9 @@
+package JavaFirstload;
+
+public class methodClass {
+    public static void main(String[] args) {
+        System.out.println("sdsz");
+        System.out.println("sdfds");
+        System.out.println("shdkufhas");
+    }
+}
