260214da91a90d246d79b323a815ac491856f3f1
Simon <sai@gmail.com>
2025-09-07 19:03:48 -0400

    6th  Commit

---
+++ src/main/java/JavaFirstload/Methods.java
@@ -0,0 +1,11 @@
+package JavaFirstload;
+
+public class Methods {
+    public void method() {
+        System.out.println("yo");
+    }
+    public static void main(String[] args) {
+        Methods con = new Methods();
+        con.method();
+    }
+}
